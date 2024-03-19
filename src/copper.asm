fcc656afdd22fc90df6ceecf481bd04e94036cfa
em00k <david.saphier@gmail.com>
2024-03-19 14:25:19 +0000

    modified:   src/copper.asm 	modified:   src/copper_settings.asm

--- src/copper.asm
+++ src/copper.asm
@@ -2,38 +2,38 @@
 ; copper routines
 
 start_copper:
-		; hl = copper list
-		; bc = length
+        ; hl = copper list
+        ; bc = length
 
-		nextreg COPPER_CONTROL_HI_NR_62, 0	; stop
-		nextreg COPPER_CONTROL_LO_NR_61, 0
+        nextreg COPPER_CONTROL_HI_NR_62, 0	; stop
+        nextreg COPPER_CONTROL_LO_NR_61, 0
 
-		push 	bc
-		ld 		bc, TBBLUE_REGISTER_SELECT_P_243B
-		ld 		a, COPPER_DATA_NR_60
-		out 	(c),a
-		pop 	bc	; select copper data port
-		call 	TransferDMAPort
+        push 	bc
+        ld 	bc, TBBLUE_REGISTER_SELECT_P_243B
+        ld	a, COPPER_DATA_NR_60
+        out 	(c),a
+        pop 	bc	; select copper data port
+        call 	TransferDMAPort
 
-		nextreg COPPER_CONTROL_HI_NR_62, %11000000 ; start
-		nextreg COPPER_CONTROL_LO_NR_61, %00000000
-		ret
+        nextreg COPPER_CONTROL_HI_NR_62, %11000000 ; start
+        nextreg COPPER_CONTROL_LO_NR_61, %00000000
+        ret
 
 stop_copper:
-		nextreg COPPER_CONTROL_HI_NR_62, 0	; stop
-		nextreg COPPER_CONTROL_LO_NR_61, 0
-		ret
+        nextreg COPPER_CONTROL_HI_NR_62, 0	; stop
+        nextreg COPPER_CONTROL_LO_NR_61, 0
+        ret
 
 update_copper:
         
-		
-		ld		a, (init_copper.layer2_xoffset-1)
-		inc     a   
-		
-		ld      (init_copper.layer2_xoffset-1),a
+                
+        ld      a, (init_copper.layer2_xoffset-1)
+        inc     a   
+        
+        ld      (init_copper.layer2_xoffset-1),a
 
-		call    init_copper
-		ret 
+        call    init_copper
+        ret 
 .xpos: 
         db      0
 
--- src/copper_settings.asm
+++ src/copper_settings.asm
@@ -4,15 +4,15 @@
 COPPER_WAIT				= %10000000
 
 init_copper:
-		ld		bc, .copperend-.copper
-		ld		hl, .copper
-		jp		start_copper	
+        ld      bc, .copperend-.copper
+        ld      hl, .copper
+        jp      start_copper	
 .copper
-		db		COPPER_WAIT, 0,  LAYER2_XOFFSET_NR_16 ,0
-		db		COPPER_WAIT, 192-8,  LAYER2_XOFFSET_NR_16 ,0
+        db      COPPER_WAIT, 0,  LAYER2_XOFFSET_NR_16 ,0
+        db      COPPER_WAIT, 192-8,  LAYER2_XOFFSET_NR_16 ,0
 .layer2_xoffset:
 
-		db		COPPER_WAIT, 192, LAYER2_XOFFSET_NR_16,0
+        db      COPPER_WAIT, 192, LAYER2_XOFFSET_NR_16,0
 
-		dw		$ffff
+        dw      $ffff
 .copperend
